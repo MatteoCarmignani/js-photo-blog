bb86cba37fe65ae18bc8f30de673125f11bb1b45
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-17 12:11:06 +0200

    completamento

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const contOutput = document.querySelector(".container");
-console.log(contOutput);
+const contOutput = document.querySelector(".container-principale");
+
+axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
+    const posts = response.data;
+
+    let postString = "";
+
+    posts.forEach(post => {
+        postString += `
+        <div class="card">
+            <img src="${post.url}" alt="Immagine 1">
+            <p>${post.title}</p>
+        </div>
+        `;
+    });
+
+    contOutput.innerHTML = postString;
+});
+
+
