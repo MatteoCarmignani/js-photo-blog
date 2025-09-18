f4140a4fb12b53f9cfcd4043b1aa823d84479345
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-18 11:56:41 +0200

    completamento bonus + overlay

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 const contOutput = document.querySelector(".container-principale");
+const overlay = document.querySelector(".overlay");
+const button = document.getElementById("bottone");
+const overlayImmagine = document.getElementById("overlay-immagine")
 
+// API
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
+
     const posts = response.data;
 
+    //Stringa vuota dove inserire card
     let postString = "";
 
     posts.forEach(post => {
         postString += `
         <div class="card">
             <img src="${post.url}" alt="immagine">
-            <p>${post.title}</p>
+            <p>${post.date}</p>
+            <h3>${post.title}<h3>
         </div>
         `;
     });
 
+    //sposto tutto in html
     contOutput.innerHTML = postString;
-});
 
+    // PARTE 2
+    
+    document.querySelectorAll(".card").forEach(card => {
+        card.addEventListener("click", () => {
+        //alert("Funziona!")
+        overlay.style.display = "flex";
+    });
+
+    bottone.addEventListener("click", () => {
+    overlay.style.display = "none";
+    });
+    
+    });
+});
 
